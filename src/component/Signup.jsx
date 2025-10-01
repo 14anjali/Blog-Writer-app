42c1c6e1abc9620daa94c207148842e6c839fba8
anjali <1424anjali@gmail.com>
2025-10-01 18:58:52 +0530

    add component files

---
+++ src/component/Signup.jsx
@@ -0,0 +1,34 @@
+import React,{useStae} from 'react'
+import authService from '../appWrite/auth'
+import { Link,useNavigate } from 'react-router-dom'
+import {login} from '../store/authSlice'
+import {Button,Input,Logo} from './index'
+import { useDispatch } from 'react-redux'
+import {useForm} from 'react-hook-form'
+
+function Signup(){
+const navigate =useNavigate()
+const [error,setError] = useStae('')
+const dispatch =useDispatch()
+const {register,handleSubmit} = useForm()
+
+const create =async(data)=>{
+    setError("")
+    try{
+         const userData=await authService.createAccount(data)
+         if(userData){
+            const userData =await authService.
+            getCurrentUser()
+            if(userData) dispatch(login(userData))
+
+            }
+    }catch(error){
+        setError(error.message)
+    }
+}
+
+    return(
+        <div>Signup</div>
+    )
+}
+export default Signup 
